3cd9e2c530f05b1545c1267fc8999b3912c551a5
Naga Nachiappan K R <Naga.KR@gds.ey.com>
2020-08-12 19:17:05 +0530

    Password Generator first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PasswordGenerator';
+ password='';
+ IncludeLetters=false;
+ IncludeNumbers=false;
+ IncludeSymbols=false;
+ length=0;
+  OnButtonClick(){
+   const Numbers='0123456789';
+   const Letters='abcdefghijklmnopqrstuvwxyz';
+   const Symbols='!@#$%^&*()';
+   let ValidChars='';
+   if(this.IncludeLetters){
+     ValidChars +=Letters;
+   }
+   if(this.IncludeNumbers){
+     ValidChars+=Numbers;
+   }
+   if(this.IncludeSymbols){
+     ValidChars+=Symbols;
+   }
+   let generatedPassword='';
+   for(let i=0;i<this.length;i++){
+     const index = Math.floor(Math.random()*ValidChars.length)
+     generatedPassword+=ValidChars[index];
+   }
+   this.password=generatedPassword;
+   
+}
+onChangeLetters(){
+this.IncludeLetters=!this.IncludeLetters;
+}
+onChangeNumbers(){
+  this.IncludeNumbers=!this.IncludeNumbers;
+}
+onChangeSymbols(){
+  this.IncludeSymbols=!this.IncludeSymbols;
+}
+onChangeLength(value: string){
+const parsedValue=parseInt(value);
+if(!isNaN(parsedValue)){
+  this.length=parsedValue;
+}
+else{
+  this.length=0;
+}
+}
+
 }
